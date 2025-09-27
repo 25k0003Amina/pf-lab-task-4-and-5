6c7fbffe556fe21c44bc56147883c0b51eaa7b61
25k0003Amina <k250003@nu.edu.pk>
2025-09-27 10:04:45 -0700

    updated commit

---
+++ task 10.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int age, movie;
+    printf("Enter age: ");
+    scanf("%d", &age);
+
+    if (age < 12) {
+        printf("Child Ticket\n");
+    } else if (age <= 60) {
+        printf("Adult Ticket\n");
+    } else {
+        printf("Senior Citizen Ticket\n");
+    }
+
+    printf("Select Movie:\n1. Action\n2. Comedy\n3. Horror\n");
+    scanf("%d", &movie);
+
+    switch (movie) {
+        case 1: printf("Movie booked: Action\n"); 
+		break;
+        case 2: printf("Movie booked: Comedy\n");
+		 break;
+        case 3: printf("Movie booked: Horror\n"); 
+		break;
+        default: printf("Invalid choice\n");
+    }
+    return 0;
+}
+
